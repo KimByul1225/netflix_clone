a417067dfadf925cf7c0bf27b2a1f4368588d729
ByulKim <kimbyul1225@gmail.com>
2022-12-04 19:25:43 +0900

    api 정리 완료

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -1,26 +1,161 @@
 const API_KEY = "ebbf66e74bb3cfe2fa2f94d37ab8ebb5";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
-interface IMovie {
+interface IResult {
+    // 공통
     id: number;
     backdrop_path: string;
-    poster_path: string;
-    title: string;
     overview: string;
+    release_date: string;
+    poster_path: string;
+    vote_average: string;
+    // ------ seires 정보
+    name: string;
+    // ------ movies 정보
+    title?: string;
 }
 
-export interface IGetMoviesResult {
+export interface IGetResult {
     dates: {
         maximum: string;
         minimum: string;
     };
     page: number;
-    results: IMovie[];
+    results: IResult[];
     total_pages: number;
     total_results: number;
 }
 
+export interface IGetDetail {
+    // 공통
+    id: number;
+    backdrop_path: string;
+    poster_path: string;
 
-export function getMovies() {
-    return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then((response) => response.json());
+    overview: string;
+    vote_average: number;
+    genres: [
+        {
+            id: number;
+            name: string;
+        }
+    ];
+    // ------ movies 정보
+    title: string;
+    original_title?: string;
+    // ------ seires 정보
+    name: string;
+    original_name?: string;
+    first_air_date: string;
+    release_date: string;
 }
+
+export interface IGetCredit {
+    // 공통
+    id: number;
+    cast: [
+        {
+            id: number;
+            name: string;
+            original_name: string;
+            character: string;
+        }
+    ];
+    crew: [
+        {
+            id: number;
+            known_for_department: string;
+            name: string;
+        }
+    ];
+}
+
+export interface ISearchResult {
+    id: number;
+    name?: string;
+    title?: string;
+    backdrop_path: string;
+    poster_path: string;
+    vote_average: number;
+    overview: string;
+    original_title: string;
+    release_date?: string; // 영화
+    first_air_date?: string; // 시리즈
+}
+
+export interface IGetSearch {
+    page: number;
+    results: ISearchResult[];
+    total_pages: number;
+    total_results: number;
+    dates: string;
+}
+
+export function getMovies(category: string) {
+    return fetch(
+        `${BASE_PATH}/movie/${category}?api_key=${API_KEY}&language=en-US&page=1`
+    ).then(response => {
+        return response.json();
+    });
+}
+
+export function getMovieDetail(id: string) {
+    return fetch(
+        `${BASE_PATH}/movie/${id}?api_key=${API_KEY}&language=en-US`
+    ).then(response => {
+        return response.json();
+    });
+}
+
+export function getMovieCredit(id: string) {
+    return fetch(
+        `${BASE_PATH}/movie/${id}/credits?api_key=${API_KEY}&language=en-US`
+    ).then(response => {
+        return response.json();
+    });
+}
+
+// -------- Series(tv)
+export function getSeries(tvCategory: string) {
+    return fetch(
+        `${BASE_PATH}/tv/${tvCategory}?api_key=${API_KEY}&language=en-US&page=1`
+    ).then(response => {
+        return response.json();
+    });
+}
+
+export function getSeriesDetail(tv_id: string) {
+    return fetch(
+        `${BASE_PATH}/tv/${tv_id}?api_key=${API_KEY}&language=en-US`
+    ).then(response => {
+        return response.json();
+    });
+}
+
+export function getSeriesCredit(tv_id: string) {
+    return fetch(
+        `${BASE_PATH}/tv/${tv_id}/credits?api_key=${API_KEY}&language=en-US`
+    ).then(response => {
+        return response.json();
+    });
+}
+
+
+// -------- Search movie
+export function getSearchMovie(keyword: string) {
+    return fetch(
+        `${BASE_PATH}/search/movie?api_key=${API_KEY}&language=en-US&query=${keyword}&page=1`
+    ).then(response => {
+        return response.json();
+    });
+}
+
+// -------- Search Tv
+export function getSearchTv(keyword: string) {
+    return fetch(
+        `${BASE_PATH}/search/tv?api_key=${API_KEY}&language=en-US&query=${keyword}&page=1&include_adult=false`
+    ).then(response => {
+        return response.json();
+    });
+}
+

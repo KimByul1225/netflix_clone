25305ef6cf9369d365bbf4bcb0ad61be84ee4d4c
ByulKim <kimbyul1225@gmail.com>
2023-01-01 11:20:21 +0900

    검색결과 라우팅처리

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './Routes/Home';
 import Header from './Components/common/Header';
 import Footer from './Components/common/Footer';
 import Series from './Routes/Series';
+import Search from './Routes/Search';
 
 
 function App() {
@@ -19,7 +20,10 @@ function App() {
         <Route path="/series" element={<Series />}>
           <Route path=":tv_id" element={<Series />} />
         </Route>
-
+        <Route path="/search" element={<Search />}>
+          <Route path="movie/:movieId" element={<Search />} />
+          <Route path="series/:tv_id" element={<Search />} />
+        </Route>
       </Routes>
       <Footer/>
     </BrowserRouter>
